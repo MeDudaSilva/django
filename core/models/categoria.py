a5e538b8f20eacf6702fd5436498c43a3bedbcb7
MeDudaSilva <mariaeduarda.11.me4@gmail.com>
2024-03-11 11:33:49 -0300

    teste api, consertando categorias

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -4,6 +4,6 @@ from django.db import models
 class Categoria(models.Model):
     descricao = models.CharField(max_length = 100, unique=True)
      
-     def __str__(self):
+    def __str__(self):
         # return self.descricao 
-return f"{self.descricao} ({self.id})"
+        return f"{self.descricao} ({self.id})"
